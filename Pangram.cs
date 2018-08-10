dee2671de1527082344e019e2cee0d67001a25c8
Schenz <bschenz@gmail.com>
2018-08-10 11:11:50 -0400

    final solution

--- Pangram.cs
+++ Pangram.cs
@@ -1,9 +1,7 @@
-﻿using System;
+﻿using System.Collections.Immutable;
 
 public static class Pangram
 {
-    public static bool IsPangram(string input)
-    {
-        throw new NotImplementedException();
-    }
+    private static readonly ImmutableHashSet<char> alphabet = ImmutableHashSet<char>.Empty.Union("abcdefghijklmnopqrstuvwxyz");
+    public static bool IsPangram(string input) => alphabet.Intersect(input.ToLower()).Count == alphabet.Count;
 }
